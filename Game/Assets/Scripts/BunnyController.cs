f1db06d59f9b7739d1e76514afc160aa2c1a3cb7
SaloniSS <sxs170015@utdallas.edu>
2020-05-30 06:38:37 -0500

    Keep Sprite on Screen

--- Game/Assets/Scripts/BunnyController.cs
+++ Game/Assets/Scripts/BunnyController.cs
@@ -19,11 +19,24 @@ public class BunnyController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    private Vector2 leftBottom;
+    private Vector2 rightTop;
+    private SpriteRenderer spriteRenderer;
+    private Vector2 spriteSize;
+    private Vector2 spriteHalfSize;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+
+      	leftBottom = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        rightTop   = Camera.main.ViewportToWorldPoint(Vector3.one);
+ 
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteSize     = spriteRenderer.sprite.bounds.size;
+        spriteHalfSize = spriteRenderer.sprite.bounds.extents;
     }
 
     // Update is called once per frame
@@ -47,6 +60,40 @@ public class BunnyController : MonoBehaviour
         position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+    }
+
+    private void LateUpdate()
+    {
+        // get the sprite's edge positions
+        float spriteLeft   = transform.position.x - spriteHalfSize.x;
+        float spriteRight  = transform.position.x + spriteHalfSize.x;
+        float spriteBottom = transform.position.y - spriteHalfSize.y;
+        float spriteTop    = transform.position.y + spriteHalfSize.y;
+ 
+        // initialize the new position to the current position
+        Vector3 clampedPosition = transform.position;
+ 
+        // if any of the edges surpass the camera's bounds,
+        // set the position TO the camera bounds (accounting for sprite's size)
+        if(spriteLeft < leftBottom.x)
+        {
+            clampedPosition.x = leftBottom.x + spriteHalfSize.x;
+        }
+        else if(spriteRight > rightTop.x)
+        {
+            clampedPosition.x = rightTop.x - spriteHalfSize.x;
+        }
+ 
+        if(spriteTop < leftBottom.y)
+        {
+            clampedPosition.y = leftBottom.y + spriteHalfSize.y;
+        }
+        else if(spriteTop > rightTop.y)
+        {
+            clampedPosition.y = rightTop.y - spriteHalfSize.y;
+        }
+ 
+        transform.position = clampedPosition;
     }
 
     public void ChangeHealth(int amount)
