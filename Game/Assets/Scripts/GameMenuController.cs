4e350002f69fafb3a6b287c1a43dd46e69a5890c
SaloniSS <sxs170015@utdallas.edu>
2020-05-30 18:47:40 -0500

    Failed attempt to add menu button to settings page

--- Game/Assets/Scripts/GameMenuController.cs
+++ Game/Assets/Scripts/GameMenuController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuScript : MonoBehaviour
+public class GameMenuController : MonoBehaviour
 {
 	public void ChangeScene(string sceneName)
 	{
