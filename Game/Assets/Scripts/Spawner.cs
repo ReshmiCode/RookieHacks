702626f031e1cb7f36e56c1b37b4b593cc136ba0
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-05-30 11:49:34 -0500

    add in trash spawner+remove

---
+++ Game/Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject trash;
+    float randY;
+    Vector2 whereToSpawn;
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            randY = Random.Range(-3.95f, 2.13f);
+            whereToSpawn = new Vector2(transform.position.x, randY);
+            Instantiate(trash, whereToSpawn, Quaternion.identity);
+        }
+    }
+}
--- Game/Assets/Scripts/TrashController.cs
+++ Game/Assets/Scripts/TrashController.cs
@@ -6,11 +6,13 @@ public class TrashController : MonoBehaviour
 {
     public float speed = 2.0f;
     Rigidbody2D rigidbody2D;
+    Vector2 leftBottom;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        leftBottom = Camera.main.ViewportToWorldPoint(Vector3.zero);
     }
 
     void FixedUpdate()
@@ -18,6 +20,14 @@ public class TrashController : MonoBehaviour
         Vector2 position = rigidbody2D.position;
         position.x = position.x - Time.deltaTime * speed;
         rigidbody2D.MovePosition(position);
+    }
+
+    private void LateUpdate()
+    {
+        if (rigidbody2D.position.x < leftBottom.x)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
