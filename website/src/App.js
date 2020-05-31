e5bfab2dc9546750394f0287b890b9c7121b70cd
SaloniSS <sxs170015@utdallas.edu>
2020-05-31 08:32:25 -0500

    Change video

--- website/src/App.js
+++ website/src/App.js
@@ -17,7 +17,7 @@ function App() {
         <iframe
           width="840"
           height="473"
-          src="https://www.youtube.com/embed/FnMZTbqBy2s"
+          src="https://www.youtube.com/embed/kU_pBXSsnk4"
           frameborder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
